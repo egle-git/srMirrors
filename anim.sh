8575cebd0f153aa71ec9039705cb9ef53d0b6b8d
Andrius Juodagalvis <andrius.juodagalvis@tfai.vu.lt>
2020-12-07 22:12:15 +0200

    veidrodziu vaizdavimas

---
+++ anim.sh
@@ -0,0 +1,1 @@
+convert -delay 100 -loop 0 dir-pics2/ntp20_fixed_cpframe_*png animatedCPNtp20StationaryFrame.gif
---
+++ run2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p dir-pics2
+
+i=0
+imax=18 # orig
+imax=8
+move=0
+while [ ${i} -lt ${imax} ] ; do
+    root -l -q -b drawMirror2.C+"($i,1,${move})"
+    i=$((i+1))
+done
