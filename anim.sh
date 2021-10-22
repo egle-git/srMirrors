728924de208058307d82efc612ec028becf01d45
Andrius Juodagalvis <andrius.juodagalvis@tfai.vu.lt>
2021-10-22 12:16:35 +0300

    added img viewer

--- anim.sh
+++ anim.sh
@@ -1,1 +1,7 @@
-convert -delay 100 -loop 0 dir-pics2/ntp20_fixed_cpframe_*png animatedCPNtp20StationaryFrame.gif
+destGif=animatedCPNtp20StationaryFrame.gif
+convert -delay 100 -loop 0 dir-pics2/ntp20_fixed_cpframe_*png ${destGif}
+
+if [ -e ${destGif} ] ; then
+  eog ${destGif} &
+fi
+
